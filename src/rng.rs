59b0bc66f79fc5f646e4193c401dfade30542352
Gustav <sir.gustav.the.coder@gmail.com>
2015-01-11 21:01:49 +0100

    added some documentation

--- src/rng.rs
+++ src/rng.rs
@@ -1,6 +1,18 @@
 extern crate std;
-// extern crate time;
 
+/// This is a RNG called WELL512, designed by the designers of Mersenne, developed 10 years later,
+/// and an all around better choice for games. The code is put in the public domain
+/// by Dr. Chris Lomont. He claims this implementation is 40% faster than Mersenne,
+/// does not suffer from poor diffusion and trapping when the state contains many 0
+/// bits, and is clearly a lot simpler code. It has a period of 2^512; a PC takes
+/// over 10^100 years to cycle through the states, so it is large enough.
+/// Here is a paper overviewing PRNGs where I found the WELL512 implementation.
+/// http://www.lomont.org/Math/Papers/2008/Lomont_PRNG_2008.pdf
+/// So - faster, simpler, created by the same designers 10 years later, and produces
+/// better numbers than Mersenne. How can you go wrong? :)
+/// 
+/// For more information please see http://stackoverflow.com/a/1227137
+/// and http://stackoverflow.com/questions/1046714/what-is-a-good-random-number-generator-for-a-game
 pub struct Rng {
 	index : usize,
 	state : [u32; 16],
@@ -9,6 +21,7 @@ pub struct Rng {
 }
 
 impl Rng {
+	/// Creates a new generator based on a seed
 	pub fn new(seed:u32) -> Rng {
 		let mut r = Rng {
 			index: 0,
@@ -29,6 +42,7 @@ impl Rng {
 	// 	return Rng::new(ct.nsep as u32);
 	// }
 
+	/// Generate a new random number
 	pub fn next(&mut self) -> u32 {
 		let mut a = self.state[self.index];
 		let mut c = self.state[(self.index + 13) & 15];
@@ -44,6 +58,7 @@ impl Rng {
 		self.state[self.index]
 	}
 
+	/// Generate a new random float between 0 and 1
 	pub fn next01(&mut self) -> f32 {
 		self.next() as f32 / std::u32::MAX as f32
 	}
